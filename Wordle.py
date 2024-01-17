2f03d8f2e15e29c342be901c7ccc0d03915f60de
reedstew <rcstew120@gmail.com>
2024-01-16 18:30:09 -0700

    choose word

--- Wordle.py
+++ Wordle.py
@@ -17,8 +17,24 @@ def wordle():
     def enter_action(s):
         gw.show_message("You have to implement this method.")
 
+    # Reed, 1/16/2024, 6pm: Defined function to call random word from by index number (0 - length of list minus one) from wordle list
+    def choose_a_word():
+        num = random.randint(0,(len(FIVE_LETTER_WORDS)-1))
+        wotd = FIVE_LETTER_WORDS[num]
+        return wotd
+    
+    def display_word():
+        word = choose_a_word()
+        i = 1
+        for letter in word:
+            print(letter)
+            gw.set_square_letter(0, N_COLS - i, letter)
+            i += 1
+
     gw = WordleGWindow()
+    display_word()
     gw.add_enter_listener(enter_action)
+
 
 # Startup code
 
